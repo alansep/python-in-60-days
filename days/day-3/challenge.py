92f374472c50ab55ada965fbeba0a4245a0954e8
Gabriel Alan <alang.von@gmail.com>
2026-05-04 23:25:42 -0300

    feat: replace completed challenges with template iris-challenge.py

--- days/day-3/challenge.py
+++
@@ -1,8 +0,0 @@
-numbers = list(range(1,21))
-
-filtered_numbers = [s * s for s in numbers if s % 3 == 0]
-
-filtered_dict = {s: "Par" if s%2==0 else "Impar" for s in filtered_numbers}
-
-print(filtered_numbers)
-print(filtered_dict)
---
+++ days/day-3/iris-challenge.py
@@ -0,0 +1,31 @@
+"""
+Iris Challenge - Day 3: Mastering Comprehensions
+
+Your task is to demonstrate your mastery of Python comprehensions. 
+Follow the instructions below to complete the challenge.
+"""
+
+# 1. LIST COMPREHENSION:
+# Create a list named 'cube_odds' that contains the CUBE (n**3) 
+# of all ODD numbers between 1 and 30.
+# TODO: Write your code here
+
+
+# 2. DICTIONARY COMPREHENSION:
+# Given the list of names below:
+names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"]
+
+# Create a dictionary named 'name_length_map' where:
+# - Key: The name (string)
+# - Value: The length of the name (int)
+# Only include names that have MORE than 3 characters.
+# TODO: Write your code here
+
+
+# 3. NESTED/CONDITIONAL CHALLENGE:
+# Create a list named 'categorized_numbers' for numbers from 1 to 20:
+# - If the number is even, the entry should be "Even-<number>"
+# - If the number is odd, the entry should be "Odd-<number>"
+# TODO: Write your code here
+
+# When finished, print all your results to verify!
